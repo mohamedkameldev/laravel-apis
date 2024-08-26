f85b05354ea78db9614b2a6e63319e3d8fa7aa98
Mohamed Mostafa <mokammel0000@gmail.com>
2024-08-26 12:41:52 +0300

    change validation in MessageController to Manually Validators

--- app/Http/Controllers/Api/MessageController.php
+++ app/Http/Controllers/Api/MessageController.php
@@ -4,30 +4,41 @@ namespace App\Http\Controllers\Api;
 
 use App\Helpers\ApiResponse;
 use App\Http\Controllers\Controller;
-use App\Http\Requests\MessageRequest;
 use App\Models\Message;
-use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class MessageController extends Controller
 {
-    /**
-     * Handle the incoming request.
-     */
-    public function __invoke(MessageRequest $request)
+    public function __invoke()
     {
-        // add request header Accept: application/json to enforce laravel return json
-        // $request->validate([
+        #------- Add request header Accept: application/json to enforce laravel return json
+        // request()->validate([
         //     'name' => 'bail|required', // to stop at the first error
         //     'email' => 'required',
         //     'message' => 'required'
         // ]);
 
-        // dd($request->all());
-        // dd($request->validated());
-        // dd($request->only(['name', 'email', 'message']));
-        // dd($request->safe()->only(['name', 'email', 'message']));
+        // $validator = Validator::validate(request()->all(), [  // returns the validated data directly
+        $validator = Validator::make(request()->all(), [        // returns an object of validator - you can get the validated data from it
+            'name' => 'required',
+            'email' => 'required|email',
+            'message' => 'required',
+        ]);
 
-        $sendMessage = Message::create($request->safe()->only(['name', 'email', 'message']));
+        if($validator->fails()) {
+            // return ApiResponse::message(0, $validator->errors(), $validator->errors()->first());
+            return ApiResponse::message(0, $validator->messages()->all(), $validator->messages()->first());
+        }
+
+        // dd(request()->validated());      // doesn't work here - works with form request validation
+        // dd(request()->all());
+        // dd(request()->only(['name', 'email', 'message']));
+        // dd(request()->safe()->only(['name', 'email', 'message']));
+
+        // dd($validator->validated());
+        // dd($validator->safe()->only(['name', 'email', 'message']));
+
+        $sendMessage = Message::create($validator->safe()->only(['name', 'email', 'message']));
 
         if($sendMessage) {
             return ApiResponse::message(status: 1, message:'your message has been send successfully');
